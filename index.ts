b4552950c73b71b086d6f5648818a1ca120add10
Bastiaan Deknudt <deknudtb@gmail.com>
2025-02-21 20:29:32 +0100

    add stack

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
-import * as github from "@pulumi/github";
+// import * as github from "@pulumi/github";
+import * as hetzner from "@pulumi/hetzner";
+import * as hcloud from "@pulumi/hcloud";
 
-const repo = new github.Repository("demo-repo", {
-    description: "Demo Repository for GitHub",
-});
+// const repo = new github.Repository("demo-repo", {
+//     description: "Demo Repository for GitHub",
+// });
+//
+// export const repositoryName = repo.name
 
-export const repositoryName = repo.name
+
+
+export const zone = hetzner.getDnsZone({ name: "bastiaandeknudt.be" });
+
+export const keys = hcloud.getSshKeys();
