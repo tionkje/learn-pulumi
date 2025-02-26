513c0a53146fc910ec249248903396259407a1b9
Bastiaan Deknudt <deknudtb@gmail.com>
2025-02-26 22:15:45 +0100

    hcloud

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as hetzner from "@pulumi/hetzner";
+import * as hcloud from "@pulumi/hcloud";
 
 export const zone = hetzner.getDnsZone({ name: "bastiaandeknudt.be" });
+
+export const keys = hcloud.getSshKeys();
